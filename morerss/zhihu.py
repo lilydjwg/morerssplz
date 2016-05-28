1fa04a439a92d97bc02625aa39022fd2a52ad8bc
lilydjwg <lilydjwg@gmail.com>
2016-05-28 10:54:52 +0800

    zhihu: add image proxy support

--- morerss/zhihu.py
+++ morerss/zhihu.py
@@ -1,4 +1,4 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, quote
 import datetime
 import json
 import re
@@ -14,8 +14,26 @@ httpclient = AsyncHTTPClient()
 
 re_br_to_remove = re.compile(r'(?:<br>)+')
 re_img = re.compile(r'<img src="([^h])')
+re_zhihu_img = re.compile(r'(?<=<img rel="noreferrer" src=")https://\w+\.zhimg\.com/[^"]+(?=")')
 
 picN = iter(itertools.cycle('1234'))
+
+def proxify_url_cf(match):
+  url = match.group()
+  return 'https://images.weserv.nl/?url=ssl:' + url[8:]
+
+def proxify_url_google(match):
+  url = match.group()
+  return 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url=' + quote(url) + '&container=focus'
+
+PIC_PROXIES = {
+  'google': proxify_url_google,
+  'cf': proxify_url_cf,
+}
+
+def proxify_pic(html, pic):
+  html = re_zhihu_img.sub(PIC_PROXIES[pic], html)
+  return html
 
 def abs_img(m):
   return '<img src="https://pic%s.zhimg.com/' % next(picN) + m.group(1)
@@ -23,6 +41,7 @@ def abs_img(m):
 class ZhihuZhuanlanHandler(BaseHandler):
   @gen.coroutine
   def get(self, name):
+    pic = self.get_argument('pic', None)
     digest = self.get_argument('digest', False) == 'true'
 
     baseurl = 'http://zhuanlan.zhihu.com/' + name
@@ -31,7 +50,7 @@ class ZhihuZhuanlanHandler(BaseHandler):
     url = 'http://zhuanlan.zhihu.com/api/columns/%s/posts?limit=20' % name
     posts = yield self._get_url(url)
 
-    rss = posts2rss(url, info, posts, digest=digest)
+    rss = posts2rss(url, info, posts, digest=digest, pic=pic)
     xml = rss.to_xml(encoding='utf-8')
     self.finish(xml)
 
@@ -55,7 +74,7 @@ def process_content(text):
   text = text.replace('<img ', '<img rel="noreferrer" ')
   return text
 
-def post2rss(baseurl, post, *, digest=False):
+def post2rss(baseurl, post, *, digest=False, pic=None):
   url = urljoin(baseurl, post['url'])
   if digest:
     content = post['summary']
@@ -63,17 +82,22 @@ def post2rss(baseurl, post, *, digest=False):
     content = '<p><img src="%s"></p>' % post['titleImage'] + post['content']
   else:
     content = post['content']
+
+  content = process_content(content)
+  if pic is not None:
+    content = proxify_pic(content, pic)
+
   item = PyRSS2Gen.RSSItem(
     title = post['title'].replace('\x08', ''),
     link = url,
-    description = process_content(content),
+    description = content,
     pubDate = parse_time(post['publishedTime']),
     author = post['author']['name'],
   )
   return item
 
-def posts2rss(baseurl, info, posts, *, digest=False):
-  items = [post2rss(baseurl, p, digest=digest) for p in posts]
+def posts2rss(baseurl, info, posts, *, digest=False, pic=None):
+  items = [post2rss(baseurl, p, digest=digest, pic=pic) for p in posts]
   rss = PyRSS2Gen.RSS2(
     title = '%s - 知乎专栏' % info['name'],
     link = baseurl,
@@ -94,7 +118,7 @@ def test(url):
   url = 'http://zhuanlan.zhihu.com/api/columns/%s/posts' % column
   posts = s.get(url).json()
 
-  rss = posts2rss(url, info, posts)
+  rss = posts2rss(url, info, posts, pic='cf')
   return rss
 
 if __name__ == '__main__':
